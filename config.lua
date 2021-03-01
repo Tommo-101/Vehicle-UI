e9ead000a99bb4063a9d5056ef7882745f503d08
Tommo <tommo1590@hotmail.co.uk>
2021-03-01 17:01:58 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,26 @@
+Config = {}
+
+Config.mTitle = "Vehicle Menu"
+Config.mBG = {'shopui_title_ie_modgarage','shopui_title_ie_modgarage'} -- interaction_bgd = Blue, gradient_bgd = Black.
+
+Config.menuKey = 344
+
+--- Don't touch :)
+--- You can touch if you want to change the language?
+Config.doors = {
+    [1] = "Front Left",
+    [2] = "Front Right" ,
+    [3] = "Back Left",
+    [4] = "Back Right",
+    [5] = "Hood" ,
+    [6] = "Trunk",  
+    [7] = "Back" , 
+    [8] = "Back 2"  
+}
+
+Config.windows = {
+    [1] = "Front Left",
+    [2] = "Front Right" ,
+    [3] = "Back Left",
+    [4] = "Back Right",
+}
